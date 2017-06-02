6555de0502aaa56f31be788b7b7af94eeeb8f155
Evelyn Sharp <yihongshi90@gmail.com>
2017-06-01 18:52:36 -0600

    server init

--- app.js
+++ app.js
@@ -3,16 +3,45 @@ const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/change-me');
+mongoose.connect('mongodb://localhost/todolist-Auth');
 
 const app = express();
+
+//Auth Controller
+const auth = require('./routes/auth');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.resolve(__dirname, './client/build')));
+
+//express session
+app.use(require('express-session')({
+  secret: process.env.SESSION_SECRET || 'secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: false,
+    secure: false
+  }
+}));
+
+//Passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+const User = require('./models/user');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Auth route
+app.use('/api/auth', auth);
+
 
 app.get('*', (request, response) => {
    response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
